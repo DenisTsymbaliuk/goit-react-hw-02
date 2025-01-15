f204049c2d2ae39e58ba122804d65c1640da6dd8
Denis Tsymbaliuk <denis.tsymbaliuk95@gmail.com>
2025-01-15 21:45:59 +0100

    Description

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import { Description } from "./Description/Description.jsx";
+function App() {
+  return (
+    <div className="css.fonts">
+      <Description />
+    </div>
+  );
+}
+export default App;
